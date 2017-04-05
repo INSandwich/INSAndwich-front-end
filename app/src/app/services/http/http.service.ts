0ccd0494ef144f7dac92e3173efed6ff65c59298
Timothee38 <timothee.craig@gmail.com>
2017-04-05 10:50:32 +0200

    Create styling for more components

--- app/src/app/services/http/http.service.ts
+++ app/src/app/services/http/http.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
-//import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class HttpService {
+
+  constructor (private http: Http) {}
+
+  // T is a generic type, it can be anything, a Sandwich object for instance...
+
+  getAll<T>(route: string, body: any, params?: string): Observable<T[]> {
+    // Gotta do this, but adding the JSON body somehow
+    return this.http.get(route+params)
+                    .map(this.extractData);
+  }
+
+/*
+  getOne<T>(route:string, body: any, params?: string): Observable<T> {}
+
+  post<T>(route: string, body: any, params?: string): Observable<T> {}
+
+  put<T>(route: string, body: any, params?: string): Observable<T> {}
+
+  delete<T>(route: string, body: any, params?: string): Observable<T> {}
+*/
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || {};
+  }
+
+  private handleError(error: Response | any) {
+
+  }
 
 }
